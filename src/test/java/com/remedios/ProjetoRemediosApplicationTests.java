d817cd3727eca6ec79b19f7cd23c81d61a40e302
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-09-17 21:02:49 -0300

    first commit

---
+++ src/test/java/com/remedios/ProjetoRemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRemediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
